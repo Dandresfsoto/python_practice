b6a99bd8e42dd9e7848d5602c7b034a8ab917b3f
Ibicarreno <you@example.comibi.carreno@gmail.com>
2023-01-06 14:47:50 -0500

    <PYT4> show to user phrase invert

---
+++ PYT-4/pyt4.py
@@ -0,0 +1,10 @@
+"""
+Escribir un programa que pida al usuario que introduzca una frase 
+en la consola y muestre por pantalla la frase invertida.
+"""
+
+# Create input to enter phrase
+phrase_to_invert = input('Please, enter the phrase you want to invert =>>  ')
+
+# Show to user phrase invert
+print(phrase_to_invert[::-1])
